a174142b5d81ea1da28c77b0f444d305c6cba8bc
Moiponesharon <166400201+Moiponesharon@users.noreply.github.com>
2024-06-02 19:08:11 +0200

    updated javascript

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
